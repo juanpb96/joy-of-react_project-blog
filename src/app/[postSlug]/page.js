7a780a183dc9180dd63c4816a9e9f7c2be270af6
MasterDev <juanpb.96@gmail.com>
2025-09-14 11:11:49 -0500

    Exercise 4: Add custom component in MDXRemote for syntax highlighting
    As MDX renders code snippets using a pre tag, I override this with the custom CodeSnippet component

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -6,6 +6,7 @@ import BlogHero from "@/components/BlogHero";
 import styles from "./postSlug.module.css";
 import { loadBlogPost } from "@/helpers/file-helpers";
 import { BLOG_TITLE } from "@/constants";
+import CodeSnippet from "@/components/CodeSnippet";
 
 export async function generateMetadata({ params }) {
   const { postSlug } = await params;
@@ -28,7 +29,7 @@ async function BlogPost({ params }) {
         publishedOn={frontmatter.publishedOn}
       />
       <div className={styles.page}>
-        <MDXRemote source={content} />
+        <MDXRemote source={content} components={{ pre: CodeSnippet }} />
       </div>
     </article>
   );
