23c5e2cac9af3a6ae601b881d4fd942fdf217e44
MasterDev <juanpb.96@gmail.com>
2025-09-17 12:34:17 -0500

    Exercise 5C: Animate remainder area
    Uncomment DivisionGroupsDemo component in mdx file
    
    Use motion.div and add layoutId in the remainder section of the component to match with the layoutId of the division groups

--- src/components/DivisionGroupsDemo/DivisionGroupsDemo.js
+++ src/components/DivisionGroupsDemo/DivisionGroupsDemo.js
@@ -75,7 +75,15 @@ function DivisionGroupsDemo({
             <p className={styles.remainderHeading}>Remainder Area</p>
 
             {range(remainder).map((index) => {
-              return <div key={index} className={styles.item} />;
+              const layoutId = `${id}-${numOfItems - (index + 1)}`;
+
+              return (
+                <motion.div
+                  key={layoutId}
+                  layoutId={layoutId}
+                  className={styles.item}
+                />
+              );
             })}
           </div>
         )}
